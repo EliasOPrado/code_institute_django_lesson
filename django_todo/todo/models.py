1a1ccc79adbb7b4f2016f0d4c348a7729a965841
EliasOPrado <eliaspradoprofessional@outlook.com>
2019-12-23 23:25:28 +0000

    added form and forms.py

--- django_todo/todo/models.py
+++ django_todo/todo/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
+    
     name = models.CharField(max_length=30, blank=False)
-    item = models.BooleanField(blank=False, default=False)
+    done = models.BooleanField(blank=False, default=False)
 
     def __str__(self):
         return self.name
