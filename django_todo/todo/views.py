092227f3adb7e1cfebcc09c804312657ec51b97a
EliasOPrado <eliaspradoprofessional@outlook.com>
2019-12-23 16:53:49 +0000

    added jinja logic within todo_list.html template

--- django_todo/todo/views.py
+++ django_todo/todo/views.py
@@ -5,6 +5,6 @@ from .models import Item
 
 
 def get_todo_list(request):
-    results = Items.objects.all()
-    return render(request, "todo_list.html", {'item':results})
+    results = Item.objects.all()
+    return render(request, "todo_list.html", {'items':results})
 
