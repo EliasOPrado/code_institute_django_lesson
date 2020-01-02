41c2e26606adecfe28bb68e779152dd42eca5f8a
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-01-02 01:37:07 +0000

    prepare settings and requiments to deploy

--- django_todo/django_todo/settings.py
+++ django_todo/django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,13 +76,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#THIS IS THE DEFAULT DATABASE OF DJANGO
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+#THIS IS THE HEROKU DEPLOYMENT DB
+DATABASES = {'default': dj_database_url.parse('postgres://cwzqevqvodqsto:72480138f15b4d17e2f4a903409a59adce752f0e2c438b116ab415a66f5263a1@ec2-174-129-32-212.compute-1.amazonaws.com:5432/defpdmhmg9b7gp')}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
